e3241c174f9909e2add6a8d514ce4174e09c0d59
Tammy L Leonard-Bruton <enigmalb@outlook.com>
2019-11-16 16:00:27 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,34 @@
+
+
+
+
+
+
+
+
+ def reverse_each_word_with_each(string)
+  array = string.split(" ")
+  new_array = []
+  array.each do |string|
+    new_array << string.reverse
+  end
+  new_array.join(" ")
+end
+
+
+
+def reverse_each_word(string)
+  array = string.split(" ") 
+  new_array = []
+  array.collect do |string| 
+    new_array << string.reverse 
+  end
+  new_array.join(" ")
+end
+
+
+
+
+
+
+
