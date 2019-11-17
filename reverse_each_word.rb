63cd8f367fce278cf96594e86c45a676e0deb2b0
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-11-17 04:49:20 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 # def reverse_each_word(phrase)
 #   array = phrase.split
 #   reversed_phrase = []
@@ -30,3 +31,13 @@ def reverse_each_word(phrase)
     	element.reverse
     end.join(" ")
   end
+=======
+def reverse_each_word(phrase)
+  phrase = "I love programming"
+  reversed_phrase = []
+  phrase.each do |element|
+    reversed_phrase << phrase.reverse("#{element}")
+  end
+  reversed_phrase
+end
+>>>>>>> 74a24dad58d9c4ccc1c282537273eae59178db35
