79077e7e6845cfc1f9ba14dce32e61cd4075851a
Yamin Ghowrwal <yamin.gh@gmail.com>
2019-11-17 04:46:02 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -11,7 +11,22 @@ def reverse_each_word(phrase)
   array = phrase.split
   reversed_phrase = []
   array.each do |element|
-    reversed_phrase << array.reverse("#{element}")
-  end
-  reversed_phrase
+    reversed_phrase << element.reverse
+    end
+  reversed_phrase.join(" ")
 end
+
+# def reverse_each_word(phrase)
+#   array = phrase.split
+#   array.collect do |element|
+#     reversed_phrase << element.reverse
+#     reversed_phrase.join(" ")
+#     end
+#   end
+
+def reverse_each_word(phrase)
+  	array = phrase.split
+  	array.collect do |element|
+    	element.reverse
+    end.join(" ")
+  end
