f8551aab020038f610e0d44883a2ab5d20e20831
Maika Kailani <queenmab051@gmail.com>
2019-11-18 17:17:54 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+
+def reverse_each_word(string)
+  array = string.split(" ")
+  sentence1 = []
+  array.collect do|string|
+    sentence1 << string.reverse
+  end
+  sentence1.join(" ")
+end
+
+reverse_each_word("Hello there, and how are you?")
